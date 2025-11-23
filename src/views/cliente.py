a6208023a7e6f6aa43baae7c7719ea59e77a3d25
Elias De Nadai Nalesso Silva <ps.eliasn@aguiabranca.com.br>
2025-11-23 10:07:44 -0300

    feat: (WIP) cliente create in mongo

--- src/views/cliente.py
+++ src/views/cliente.py
@@ -3,17 +3,23 @@ from fastapi import APIRouter
 from abstract import SQLResult, SQLResultDict
 from models.cliente import Cliente
 from repositories.sql_db import aexec_query
+from repositories.mongo import Mongo
 
 
 router = APIRouter(prefix='/cliente', tags=['cliente'])
 
+COLLECTION_NAME = 'cliente'
 
-@router.post('/', response_model=SQLResult[Cliente])
-async def create(cliente: Cliente) -> SQLResultDict:
-    return await aexec_query(
-        'cliente/create.sql',
+
+# FIXME: response_model
+@router.post('/')
+async def create(cliente: Cliente):
+    app_adb = Mongo().app_adb
+    collection = app_adb[COLLECTION_NAME]
+    res = collection.insert_one(
         cliente.model_dump(),
     )
+    return res
 
 
 @router.get('/', response_model=SQLResult[Cliente])
