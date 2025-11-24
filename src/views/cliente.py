e973d6783d7370652ac167767ba666f40d3bc2ee
Elias De Nadai Nalesso Silva <ps.eliasn@aguiabranca.com.br>
2025-11-24 07:38:52 -0300

    fix: path operation functions type annotation

--- src/views/cliente.py
+++ src/views/cliente.py
@@ -11,25 +11,28 @@ service = ClienteService()
 
 
 @router.post('/')
-async def create(cliente: Cliente):
+async def create(cliente: Cliente) -> dict:
     return await service.create(cliente)
 
 
 @router.get('/')
-async def read_all():
+async def read_all() -> list[Cliente]:
     return await service.read_all()
 
 
 @router.get('/{_id}')
-async def read_one(_id: str):
+async def read_one(_id: str) -> Cliente | None:
     return await service.read_one(_id)
 
+
 @router.put('/{_id}')
-async def update(_id: str, cliente: Cliente):
+async def update(_id: str, cliente: Cliente) -> Cliente:
     return await service.update(_id, cliente)
+
 
 # TODO: Partial update with .patch
 
+
 @router.delete('/{_id}')
-async def delete(_id: str):
+async def delete(_id: str) -> Cliente:
     return await service.delete(_id)
