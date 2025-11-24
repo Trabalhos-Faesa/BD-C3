0c14a21a5f025463a8569d7364c5fb19dba87cb1
Elias De Nadai Nalesso Silva <ps.eliasn@aguiabranca.com.br>
2025-11-24 06:12:20 -0300

    refactor(cliente): segregate app_layer with ClienteService

---
+++ src/app_layer/cliente.py
@@ -0,0 +1,44 @@
+from bson import ObjectId
+from fastapi import HTTPException
+from pymongo import ReturnDocument
+
+from models.cliente import Cliente
+from repositories.mongo import Mongo
+
+
+class ClienteService:
+    def __init__(self) -> None:
+        self.collection_name = 'cliente'
+        self.acoll = Mongo().get_acoll(self.collection_name)
+
+    async def create(self, cliente: Cliente) -> dict:
+        res = await self.acoll.insert_one(
+            cliente.model_dump(),
+        )
+        return {
+            'inserted_id': str(res.inserted_id),
+            'acknowledged': res.acknowledged,
+        }
+
+    async def read_all(self) -> list[Cliente]:
+        return [c async for c in self.acoll.find({})]
+
+    async def read_one(self, _id: str) -> Cliente | None:
+        # FIXME: where does not exists
+        return await self.acoll.find_one({"_id": ObjectId(_id)})
+
+    async def update(self, _id: str, cliente: Cliente) -> Cliente:
+        return await self.acoll.find_one_and_update(
+            {'_id': ObjectId(_id)},
+            {
+                '$set': cliente.model_dump() | {
+                    '_id': ObjectId(_id),
+                },
+            },
+            return_document=ReturnDocument.AFTER,
+        )
+
+    async def delete(self, _id: str) -> Cliente:
+        if (await self.read_one(_id)) is None:
+            raise HTTPException(404, f"_id '{_id}' does not exists")
+        return await self.acoll.find_one_and_delete({'_id': ObjectId(_id)})
--- src/views/cliente.py
+++ src/views/cliente.py
@@ -1,61 +1,35 @@
-# FIXME: router response_model e return types
+# TODO: router response_model and better return types
 
-from bson import ObjectId
-from fastapi import APIRouter, HTTPException
-from pymongo import ReturnDocument
+from fastapi import APIRouter
 
+from app_layer.cliente import ClienteService
 from models.cliente import Cliente
-from repositories.mongo import Mongo
 
 
 router = APIRouter(prefix='/cliente', tags=['cliente'])
-
-coll = Mongo().get_acoll('cliente')
+service = ClienteService()
 
 
 @router.post('/')
 async def create(cliente: Cliente) -> dict:
-    res = await coll.insert_one(
-        cliente.model_dump(),
-    )
-    return {
-        'inserted_id': str(res.inserted_id),
-        'acknowledged': res.acknowledged,
-    }
+    return await service.create(cliente)
 
 
 @router.get('/')
 async def read_all() -> list[Cliente]:
-    res = [c async for c in coll.find({})]
-    return res
+    return await service.read_all()
 
 
 @router.get('/{_id}')
 async def read_one(_id: str) -> Cliente:
-    res = await coll.find_one({"_id": ObjectId(_id)})
-    return res
+    return await service.read_one(_id)
 
-
-# TODO: Partial update with .patch
 @router.put('/{_id}')
 async def update(_id: str, cliente: Cliente) -> Cliente:
-    res = await coll.find_one_and_update(
-        {'_id': ObjectId(_id)},
-        {
-            '$set': cliente.model_dump() | {
-                '_id': ObjectId(_id),
-            },
-        },
-        return_document=ReturnDocument.AFTER,
-    )
-    return res
+    return await service.update(_id, cliente)
 
+# TODO: Partial update with .patch
 
 @router.delete('/{_id}')
 async def delete(_id: str) -> Cliente:
-    # TODO: DRY
-    exists: bool = (await coll.find_one({"_id": ObjectId(_id)})) is not None
-    if not exists:
-        raise HTTPException(404, f"_id '{_id}' does not exists")
-    res = await coll.find_one_and_delete({'_id': ObjectId(_id)})
-    return res
+    return await service.delete(_id)
