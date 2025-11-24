2a6be3f7ac0e7712122941919cbf6ef4d6180743
Elias De Nadai Nalesso Silva <ps.eliasn@aguiabranca.com.br>
2025-11-24 08:23:17 -0300

    feat: use utils.pydantic in the remaining models

--- src/models/item_carrinho.py
+++ src/models/item_carrinho.py
@@ -1,12 +1,23 @@
 from typing import Annotated, Optional
 
-from pydantic import AfterValidator, BaseModel
+from pydantic import (
+    AfterValidator,
+    BaseModel,
+    Field,
+)
+
+from utils.pydantic import (
+    PyObjectId,
+    default_model_config,
+)
 
 
 # TODO
 class CarrinhoCompra(BaseModel):
-    id_carrinho: Optional[int] = None
+    id: Optional[PyObjectId] = Field(default=None, alias='_id')
     id_cliente: int
     id_produto: int
     data_criacao: Optional[str] = None
     quantidade: Annotated[int, AfterValidator(lambda v: v if v > 0 else 1)]
+
+    model_config = default_model_config
--- src/models/pedido.py
+++ src/models/pedido.py
@@ -1,12 +1,19 @@
 from typing import Optional
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from utils.pydantic import (
+    PyObjectId,
+    default_model_config,
+)
 
 
 class Pedido(BaseModel):
-    id_pedido: Optional[int] = None
+    id: Optional[PyObjectId] = Field(default=None, alias='_id')
     id_cliente: int
     data_pedido: Optional[str] = None
     status: str
     id_carrinho: int
     valorTotal: float
+
+    model_config = default_model_config
